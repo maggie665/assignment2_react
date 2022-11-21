b172002ec7a545aac0ecbf5e7010d66305db5c60
maggie665 <maggie665@163.com>
2022-11-22 08:58:04 +1300

    try semester

--- src/components/Semester.js
+++ src/components/Semester.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import axios from "axios";
-import * as url from "url";
 import {BaseUrl} from "./constants";
+import {useLocation} from "react-router-dom";
+
 
 function Semester(props) {
-    function createSemester(){
-        axios.post(
-            url：BaseUrl + 'semester/',
+    const [semesters, setSemesters] = useState([]);
+    // const [hasToken, setHasToken] = useState(false);
+    // const [userID, setUserID] = useState(0);
 
 
-        )
-    }
+
+    useEffect(()=>{
+        axios.get(BaseUrl+"semester/")
+            .then(response=>{
+                setSemesters(response.data)
+            }).catch(error=>{
+                console.log(error)
+        })
+    },[])
+
     return (
-        <div>
-            This is Semester page
-        </div>
+        <Fragment>
+            {semesters.map(semester=>
+                <p value={semester.id} key={semester.id}>{semester.name}</p>
+            )}
+        </Fragment>
     );
 }
+
+
+
+
+// function SemesterDetail(props) {
+//     const location=useLocation();
+//     const semester_id = location.state.semester_id
+//     const [post, setPost] = useState({});
+//     useEffect(()=>{
+//         axios.get(BaseUrl+"semester/semester_viewset/"+semester_id)
+//             .then(response=>{
+//                 setSemester(response.data)
+//             }).catch(error=>{
+//                 console.log(error)
+//         })
+//     }, [post])
+//
+//     return (
+//         <div>
+//             <h1>{post.title}</h1>
+//             <div>
+//                 {post.body}
+//             </div>
+//         </div>
+//     );
+// }
+
+
 
 export default Semester;
